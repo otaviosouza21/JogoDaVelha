321af945814bb10a02b551a8acdf217a648ead97
Otavio Souza <otaviosouzalu@gmail.com>
2023-06-24 13:47:08 -0300

    Jogo da Velha

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './game.css';
+const XorO = ["O","X"]
+const random = Math.floor(Math.random() * 2)
+
 
 function App() {
+  const empty = Array(9).fill('');
+  const [gameBoard, setGameBoard] = React.useState(empty);
+  const [jogador, setJogador] = React.useState(XorO[random]);
+  const [winner, setWinner] = React.useState(null);
+  const [forEmpate, setForEmpate] = React.useState(null);
+
+  React.useEffect(forWinner, [jogador]);
+
+  function handleClick(index) {
+    if (gameBoard[index] !== '' || winner !== null) return null;
+    if (jogador === 'X') setJogador('O');
+    if (jogador === 'O') setJogador('X');
+    setGameBoard(
+      gameBoard.map((cell, cellindex) => {
+        return cellindex === index ? jogador : cell;
+      }),
+    );
+  }
+
+  function empate() {
+    if (gameBoard.every((item) => item !== '')) {
+      setForEmpate(true);
+      
+    }
+  }
+
+  function forWinner() {
+    const winner = [
+      [gameBoard[0], gameBoard[1], gameBoard[2]],
+      [gameBoard[3], gameBoard[4], gameBoard[5]],
+      [gameBoard[6], gameBoard[7], gameBoard[8]],
+
+      [gameBoard[0], gameBoard[3], gameBoard[6]],
+      [gameBoard[1], gameBoard[4], gameBoard[7]],
+      [gameBoard[2], gameBoard[5], gameBoard[8]],
+
+      [gameBoard[0], gameBoard[4], gameBoard[8]],
+      [gameBoard[2], gameBoard[4], gameBoard[6]],
+    ];
+    console.log(gameBoard);
+    winner.forEach((item) => {
+      if (item.every((item) => item === 'O')) setWinner('O');
+      if (item.every((item) => item === 'X')) setWinner('X');
+    });
+ 
+    empate();
+  }
+
+  function resetGame() {
+    setGameBoard(empty);
+    setWinner(null);
+    setJogador('O');
+    setForEmpate(null);
+  }
+  const button = {
+    color: 'white',
+    background: 'green',
+    marginTop:"20px",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <h1 className="title">Jogo da Velha</h1>
+      {winner === null ? 
+      <p>
+        Vez de <span className={`vez ${jogador}`}>{jogador}</span>
+      </p>
+      :
+      null
+}
+      <section className="board">
+        {gameBoard.map((cell, index) => {
+          return (
+            <div
+              onClick={() => handleClick(index)}
+              key={index}
+              className={`fill ${cell} ${winner && "venceu"}`}
+            >
+              {cell}
+            </div>
+          );
+        })}
+
+        <footer className="footer">
+          {winner && (
+            <p>
+              Jogador <span className={`winner ${winner}`}>{winner}</span>{' '}
+              Venceu!
+            </p>
+          )}
+          {forEmpate && <p>EMPATOU</p>}
+          <button style={button} onClick={resetGame}>
+            Reiniciar
+          </button>
+        </footer>
+      </section>
+    </main>
   );
 }
 
